c02a44f773f7db75e1b0c7ddc9c918943409077a
hesham <nbilha161@gmail.com>
2026-01-22 05:40:13 +0200

    feat: enhance DashboardLayout with OnlineUsersWidget and SocketProvider for real-time user updates

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,25 +1,34 @@
+"use client";
 import AppSidebar from "@/components/dashboard/AppSidebar";
 import DashboardHeader from "@/components/dashboard/DashboardHeader";
+import OnlineUsersWidget from "@/components/dashboard/OnlineUsersWidget";
 import ProtectedRoute from "@/components/auth/ProtectedRoute";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AnimatePresence } from "framer-motion";
+import { SocketProvider } from "@/components/providers/SocketProvider";
+import { useAuth } from "@/contexts/AuthContext";
+import { OnlineUser } from "@/types/user";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { user } = useAuth();
   return (
     <ProtectedRoute>
-      <SidebarProvider defaultOpen={false}>
-        <AnimatePresence mode="wait">
-          <AppSidebar />
-          <SidebarInset className="w-[calc(100%-var(--sidebar-width))]">
-            <DashboardHeader />
-            <main className="p-4 lg:p-6">{children}</main>
-          </SidebarInset>
-        </AnimatePresence>
-      </SidebarProvider>
+      <SocketProvider>
+        <OnlineUsersWidget user={user as OnlineUser} />
+        <SidebarProvider defaultOpen={false}>
+          <AnimatePresence mode="wait">
+            <AppSidebar />
+            <SidebarInset className="w-[calc(100%-var(--sidebar-width))]">
+              <DashboardHeader />
+              <main className="p-4 lg:p-6">{children}</main>
+            </SidebarInset>
+          </AnimatePresence>
+        </SidebarProvider>
+      </SocketProvider>
     </ProtectedRoute>
   );
 }
