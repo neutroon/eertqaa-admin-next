f7a8116ddb5944dad250abc57bf9aa03ed1f34dc
hesham <nbilha161@gmail.com>
2026-04-23 22:49:35 +0200

    refactor: consolidate provider components in layout for improved structure and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
-"use client";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "@/contexts/AuthContext";
-import { Toaster } from "sonner";
-import { ProgressProvider } from "@bprogress/next/app";
-import QueryProvider from "@/components/providers/QueryProvider";
+import AppProviders from "@/components/providers/AppProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,23 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          <QueryProvider>
-            <ProgressProvider
-              height="4px"
-              options={{ showSpinner: false }}
-              shallowRouting
-            >
-              {children}
-            </ProgressProvider>
-          </QueryProvider>
-          <Toaster
-            position="top-right"
-            richColors
-            duration={7000}
-            closeButton
-          />
-        </AuthProvider>
+        <AppProviders>
+          {children}
+        </AppProviders>
       </body>
     </html>
   );
